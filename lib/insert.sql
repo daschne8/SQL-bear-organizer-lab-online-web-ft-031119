73f176dabf4f41d77a4c982e6dd3f4fd5e02621d
Daniel Schneider <daschne8@gmail.com>
2019-04-08 15:52:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr Chocolate",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",,"","","",);
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(NULL,,"","","",);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Mr Chocolate",6,"Male","Black","Nice",TRUE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Rowdy",2,"Male","Gold","Nice",TRUE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Tabitha",3,"Female","Silver","Grumpy",FALSE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Sergeant Brown",5,"Male","Brown","Bold",TRUE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Melissa",7,"Female","White","Shy",TRUE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Grinch",2,"Male","Black","Grumpy",FALSE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES("Wendy",5,"Female","Brown","Jovial",TRUE);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(NULL,12,"Male","Brown","Aggressive",TRUE);
